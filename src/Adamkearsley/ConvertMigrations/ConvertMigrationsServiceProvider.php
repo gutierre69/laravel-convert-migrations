c0ca56bce77dd1a68004b5a2729c63a256ddffbc
Carlos Gutierre <gutierre69@gmail.com>
2019-03-24 17:09:52 -0300

    Update ConvertMigrationsServiceProvider.php

--- src/Adamkearsley/ConvertMigrations/ConvertMigrationsServiceProvider.php
+++ src/Adamkearsley/ConvertMigrations/ConvertMigrationsServiceProvider.php
@@ -27,7 +27,7 @@ class ConvertMigrationsServiceProvider extends ServiceProvider {
      */
     public function register()
     {
-        $this->app->share('artisan.convert.migrations', function($app) {
+        $this->app->singleton('artisan.convert.migrations', function($app) {
             return new ConvertMigrationsCommand;
         });
 
